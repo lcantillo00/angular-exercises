6cef1dec431baaf324812d72229126283bf6a721
FVI <test@examle.com>
2017-03-30 19:57:12 -0400

    test 2 web1000

--- app/BlogPostCtrl.js
+++ app/BlogPostCtrl.js
@@ -1,11 +1,21 @@
 (function ()
 {
     'use strict';
-    function BlogPostCtrl()
+    function BlogPostCtrl($scope)
     {
+        $scope.posts=[{
+            'ID':1,
+            'Author':'Jack',
+            'Title':'Diving Deep with Dependency Injection'},
+            {'ID':2,
+            'Author':'Jill',
+             'Title':'Practical End-to-End Testing with Protractor'
+        }];
+
 
     }
 
     var module = angular.module("exerciseApp", []);
-    module.controller('BlogPostCtrl', [BlogPostCtrl]);
+    module.controller('BlogPostCtrl',['$scope',BlogPostCtrl]);
+
 })();
